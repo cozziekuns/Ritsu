68ff2c03a8ce590d84a5e5ba1c33cec188812b1f
cozziekuns <dingaling279@berkeley.edu>
2015-06-22 00:16:43 -0700

    p69

---
+++ p69/totient.rb
@@ -0,0 +1,22 @@
+primes = [2]
+i = 3
+
+(3..100).step(2) { |i|
+  is_prime = true
+  primes.each { |prime| 
+    break if not is_prime
+    break if Math.sqrt(i) < prime
+    is_prime = false if i % prime == 0
+  }
+  primes.push(i) if is_prime
+}
+
+value = 1
+next_value = primes.shift
+
+until value * next_value > 1000000
+  value *= next_value
+  next_value = primes.shift
+end
+
+p value
