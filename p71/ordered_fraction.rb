98077f6052aa8cd0b8272c89bcd537b043594694
cozziekuns <dingaling279@berkeley.edu>
2015-06-22 15:47:28 -0700

    p71

---
+++ p71/ordered_fraction.rb
@@ -0,0 +1,14 @@
+answer = 0
+min_diff = 1
+
+999999.times { |i|
+  next if (i + 1) % 7 == 0
+  numerator = ((i + 1) * 1.0 * 3 / 7).to_i
+  diff = (1.0 * 3 / 7) - (1.0 * numerator / (i + 1)) 
+  if diff < min_diff 
+    min_diff = diff
+    answer = numerator
+  end
+}
+
+p answer
