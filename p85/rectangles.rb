00340532accce830804f932b923aee3cc489246f
cozziekuns <dingaling279@berkeley.edu>
2015-06-28 17:51:19 -0700

    added comments

--- p85/rectangles.rb
+++ p85/rectangles.rb
@@ -2,11 +2,15 @@
 
 def rectangle_number(width, height)
   count = 0
-  width.times { |i|
-    height.times { |j|
-      count += (width - i) * (height - j)
-    }
-  }
+  # For example, width: 3, height: 2
+  # 1x1: (3 - 1 + 1) * (2 - 1 + 1) = 6
+  # 2x1: (3 - 2 + 1) * (2 - 1 + 1) = 4
+  # 3x1: (3 - 3 + 1) * (2 - 1 + 1) = 2
+  # 1x2: (3 - 1 + 1) * (2 - 2 + 1) = 3
+  # 2x2: (3 - 2 + 1) * (2 - 2 + 1) = 2
+  # 3x2: (3 - 3 + 1) * (2 - 2 + 1) = 1
+  # Sum of all of those is 18, which is the correct answer
+  width.times { |i| height.times { |j| count += (width - i) * (height - j) } }
   return count
 end
 
